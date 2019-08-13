f6f58f89bae4970fd751776f83a6179c8ca32466
yuanliang <ylyang@leapstack.cn>
2019-08-13 17:04:20 +0800

    服务提供者

---
+++ src/main/java/com/yyl/provider/controller/HelloController.java
@@ -0,0 +1,26 @@
+package com.yyl.provider.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yangyuanliang
+ */
+@RestController
+public class HelloController {
+    private final Logger logger=Logger.getLogger(HelloController.class);
+    @Autowired
+    private DiscoveryClient client;
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String hello(){
+        ServiceInstance serviceInstance=client.getLocalServiceInstance();
+        logger.info("/hello host:"+serviceInstance.getHost());
+        return "hello";
+    }
+
+}
